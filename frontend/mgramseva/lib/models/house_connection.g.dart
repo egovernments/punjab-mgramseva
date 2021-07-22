416873836d03aed64a2c80d81f3de5f3276528e3
krishnaji-eGov <krishnaji.yedlapalli@walkingtree.tech>
2021-07-22 12:18:54 +0530

    changes related to the house connection model

---
+++ frontend/mgramseva/lib/models/house_connection.g.dart
@@ -0,0 +1,16 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'house_connection.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+HouseConnection _$HouseConnectionFromJson(Map<String, dynamic> json) {
+  return HouseConnection()..consumerName = json['consumerName'] as String?;
+}
+
+Map<String, dynamic> _$HouseConnectionToJson(HouseConnection instance) =>
+    <String, dynamic>{
+      'consumerName': instance.consumerName,
+    };
