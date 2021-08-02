514cfbb2ad6b06a45bf7f408be5d7fb2621c1f17
RamkrishnaEgovt <ramkrishna.sahoo@egovernments.org>
2021-08-02 12:04:46 +0530

    Mgram 24

--- frontend/mgramseva/lib/providers/reset_password_provider.dart
+++ frontend/mgramseva/lib/providers/reset_password_provider.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mgramseva/repository/reset_password_repo.dart';
+import 'package:mgramseva/utils/global_variables.dart';
 import 'package:mgramseva/utils/notifyers.dart';
+import 'package:provider/provider.dart';
+
+import 'common_provider.dart';
 class ResetPasswordProvider with ChangeNotifier {
   resetpassword(BuildContext context, String otp1,String otp2,String otp3,String otp4, String newPassword) async {
 
@@ -8,12 +12,15 @@ class ResetPasswordProvider with ChangeNotifier {
     FocusScope.of(context).unfocus();
 
     try{
+      var commonProvider = Provider.of<CommonProvider>(
+          navigatorKey.currentContext!,
+          listen: false);
       var body = {
         "otpReference": otp1+otp2+otp3+otp4,
-        "userName":"9191919149",
+        "userName": commonProvider.userDetails!.userRequest!.userName,
         "newPassword": newPassword,
-        "tenantId":"pb",
-        "type":"EMPLOYEE"
+        "tenantId": commonProvider.userDetails!.userRequest!.tenantId,
+        "type": commonProvider.userDetails!.userRequest!.type
       };
 
       var resetResponse = await ResetPasswordRepository().forgotPassword(body);
--- frontend/mgramseva/lib/repository/reset_password_repo.dart
+++ frontend/mgramseva/lib/repository/reset_password_repo.dart
@@ -1,15 +1,19 @@
 import 'package:mgramseva/model/resetPassword/reset_password.dart';
+import 'package:mgramseva/providers/common_provider.dart';
 import 'package:mgramseva/services/RequestInfo.dart';
 import 'package:mgramseva/services/base_service.dart';
 import 'package:mgramseva/services/urls.dart';
+import 'package:mgramseva/utils/global_variables.dart';
 import 'package:mgramseva/utils/models.dart';
+import 'package:provider/provider.dart';
 
 class ResetPasswordRepository extends BaseService {
   Future<ResetPasswordDetails> forgotPassword(Map body) async {
-    print("repo---->");
-    print(body);
+    var commonProvider = Provider.of<CommonProvider>(
+        navigatorKey.currentContext!,
+        listen: false);
     final requestInfo = RequestInfo('Rainmaker', .01, "", "create", 1, "", "",
-        "61894cd3-6628-47f0-a322-c8c2adcf8731");
+        commonProvider.userDetails!.accessToken);
     late ResetPasswordDetails resetPasswordDetails;
     var res = await makeRequest(
         url: UserUrl.RESET_PASSWORD,
