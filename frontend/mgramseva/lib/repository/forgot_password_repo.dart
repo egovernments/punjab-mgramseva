15717f497b2144fef6d73faf23955fc0bd2c14be
RamkrishnaEgovt <ramkrishna.sahoo@egovernments.org>
2021-08-02 15:56:20 +0530

    Mgram 24 hard coded removed from Repo

--- frontend/mgramseva/lib/repository/forgot_password_repo.dart
+++ frontend/mgramseva/lib/repository/forgot_password_repo.dart
@@ -1,13 +1,19 @@
 import 'package:mgramseva/model/forgotPassword/forgot_password.dart';
+import 'package:mgramseva/providers/common_provider.dart';
 import 'package:mgramseva/services/RequestInfo.dart';
 import 'package:mgramseva/services/base_service.dart';
 import 'package:mgramseva/services/urls.dart';
+import 'package:mgramseva/utils/global_variables.dart';
 import 'package:mgramseva/utils/models.dart';
+import 'package:provider/provider.dart';
 
 class ForgotPasswordRepository extends BaseService {
   Future<ForgotPasswordOTP> forgotPassword(Map body) async {
+    var commonProvider = Provider.of<CommonProvider>(
+        navigatorKey.currentContext!,
+        listen: false);
     final requestInfo = RequestInfo('Rainmaker', .01, "", "_search", 1, "", "",
-        "61894cd3-6628-47f0-a322-c8c2adcf8731");
+        commonProvider.userDetails!.accessToken);
     late ForgotPasswordOTP forgotPasswordOTP;
     var res = await makeRequest(
         url: UserUrl.OTP_RESET_PASSWORD,
