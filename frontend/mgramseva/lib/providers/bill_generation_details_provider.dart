9d6a9950d1036fd3e6fa58022bd43c43c5a336b4
RamkrishnaEgovt <ramkrishna.sahoo@egovernments.org>
2021-08-05 00:16:34 +0530

    billgenerate model, provider

---
+++ frontend/mgramseva/lib/providers/bill_generation_details_provider.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:mgramseva/model/bill_generation_details/bill_generation_details.dart';
+import 'package:mgramseva/utils/Constants/I18KeyConstants.dart';
+import 'package:mgramseva/utils/global_variables.dart';
+import 'package:mgramseva/utils/models.dart';
+import 'package:mgramseva/utils/notifyers.dart';
+import 'package:mgramseva/widgets/CommonSuccessPage.dart';
+
+class BillGenerationProvider with ChangeNotifier {
+  var formKey = GlobalKey<FormState>();
+  var autoValidation = false;
+  var billGenerateDetails = BillGenerationDetails();
+
+onChangeOfServiceType (val){
+  print(val.toString());
+  billGenerateDetails.serviceType = val;
+  notifyListeners();
+}
+
+onChangeOfServiceCat(val){
+  print(val.toString());
+    billGenerateDetails.serviceCat = val;
+    notifyListeners();
+}
+onChangeOfProperty(val){
+  print(val.toString());
+    billGenerateDetails.propertyType = val;
+    notifyListeners();
+}
+void onChangeOfBillYear(val){
+  print(val.toString());
+    billGenerateDetails.billYear = val;
+    notifyListeners();
+}
+void onChangeOfBillCycle(val){
+  print(val.toString());
+    billGenerateDetails.billCycle = val;
+    notifyListeners();
+}
+
+  void onSubmit(context) async{
+  try {
+    if (formKey.currentState!.validate() &&
+        billGenerateDetails.serviceType == "METER CONNECTION") {
+      if (billGenerateDetails.om_1Ctrl.text.isEmpty ||
+          billGenerateDetails.om_2Ctrl.text.isEmpty ||
+          billGenerateDetails.om_3Ctrl.text.isEmpty ||
+          billGenerateDetails.om_4Ctrl.text.isEmpty ||
+          billGenerateDetails.om_5Ctrl.text.isEmpty) {
+        Notifiers.getToastMessage(
+            context, 'Old Meter Reading is Invalid', 'ERROR');
+      }
+      else if (billGenerateDetails.nm_1Ctrl.text.isEmpty ||
+          billGenerateDetails.nm_2Ctrl.text.isEmpty ||
+          billGenerateDetails.nm_3Ctrl.text.isEmpty ||
+          billGenerateDetails.nm_4Ctrl.text.isEmpty ||
+          billGenerateDetails.nm_5Ctrl.text.isEmpty) {
+        Notifiers.getToastMessage(
+            context, 'New Meter Reading is Invalid', 'ERROR');
+      }
+      else {
+        var oldMeter = billGenerateDetails.om_1Ctrl.text +
+            billGenerateDetails.om_2Ctrl.text +
+            billGenerateDetails.om_3Ctrl.text +
+            billGenerateDetails.om_4Ctrl.text +
+            billGenerateDetails.om_5Ctrl.text;
+        var newMeter = billGenerateDetails.nm_1Ctrl.text +
+            billGenerateDetails.nm_2Ctrl.text +
+            billGenerateDetails.nm_3Ctrl.text +
+            billGenerateDetails.nm_4Ctrl.text +
+            billGenerateDetails.nm_5Ctrl.text;
+        print(oldMeter);
+        if (int.parse(oldMeter) < int.parse(newMeter)) {
+          Navigator.of(context).pushReplacement(
+              new MaterialPageRoute(builder: (BuildContext context) {
+                return CommonSuccess(SuccessHandler(
+                    i18.demandGenerate.GENERATE_BILL_SUCCESS,
+                    i18.demandGenerate.GENERATE_BILL_SUCCESS_SUBTEXT,
+                    i18.common.BACK_HOME));
+              }));
+        }
+        else {
+          Notifiers.getToastMessage(
+              context,
+              'New Meter reading should be greater than old meter reading',
+              'ERROR');
+        }
+      }
+    }
+    else if (formKey.currentState!.validate() &&
+        billGenerateDetails.serviceType == "NON METER CONNECTION") {
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (BuildContext context) {
+            return CommonSuccess(SuccessHandler(
+                i18.demandGenerate.GENERATE_BILL_SUCCESS,
+                i18.demandGenerate.GENERATE_BILL_SUCCESS_SUBTEXT,
+                i18.common.BACK_HOME));
+          }));
+    }
+    else {
+      autoValidation = true;
+      notifyListeners();
+    }
+  }
+  catch (e) {
+    navigatorKey.currentState?.pop();
+  }
+}
+  List<DropdownMenuItem<Object>> getDropDownList(List<KeyValue>? list) {
+    if (list?.length != 0) {
+      return (list ?? <KeyValue>[]).map((value) {
+        return DropdownMenuItem(
+          value: value.key,
+          child: new Text(value.label),
+        );
+      }).toList();
+    }
+    return <DropdownMenuItem<Object>>[];
+  }
+
+
+}
--- frontend/mgramseva/lib/screeens/GenerateBill/GenerateBill.dart
+++ frontend/mgramseva/lib/screeens/GenerateBill/GenerateBill.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:mgramseva/model/bill_generation_details/bill_generation_details.dart';
+import 'package:mgramseva/providers/bill_generation_details_provider.dart';
 import 'package:mgramseva/screeens/GenerateBill/widgets/MeterReading.dart';
 import 'package:mgramseva/utils/Constants/I18KeyConstants.dart';
 import 'package:mgramseva/utils/constants.dart';
-import 'package:mgramseva/utils/models.dart';
-import 'package:mgramseva/utils/notifyers.dart';
 import 'package:mgramseva/utils/validators/Validators.dart';
 import 'package:mgramseva/widgets/BaseAppBar.dart';
 import 'package:mgramseva/widgets/BottonButtonBar.dart';
-import 'package:mgramseva/widgets/CommonSuccessPage.dart';
 import 'package:mgramseva/widgets/DrawerWrapper.dart';
 import 'package:mgramseva/widgets/FormWrapper.dart';
 import 'package:mgramseva/widgets/HomeBack.dart';
@@ -16,6 +14,7 @@ import 'package:mgramseva/widgets/LabelText.dart';
 import 'package:mgramseva/widgets/SelectFieldBuilder.dart';
 import 'package:mgramseva/widgets/SideBar.dart';
 import 'package:mgramseva/widgets/TextFieldBuilder.dart';
+import 'package:provider/provider.dart';
 
 class GenerateBill extends StatefulWidget {
   State<StatefulWidget> createState() {
@@ -28,105 +27,15 @@ class _GenerateBillState extends State<GenerateBill> {
   void initState() {
     super.initState();
   }
-  var serviceCat ;
-  var serviceType ;
-  var propertyType ;
-  var billYear;
-  var billCycle;
   var metVal = "";
-  var meterNumber = new TextEditingController();
-  var om_1 = new TextEditingController();
-  var om_2 = new TextEditingController();
-  var om_3 = new TextEditingController();
-  var om_4 = new TextEditingController();
-  var om_5 = new TextEditingController();
-  var nm_1 = new TextEditingController();
-  var nm_2 = new TextEditingController();
-  var nm_3 = new TextEditingController();
-  var nm_4 = new TextEditingController();
-  var nm_5 = new TextEditingController();
 
-
-  final formKey = GlobalKey<FormState>();
-  var autoValidation = false;
-  _onSubmit(context) {
-    if (formKey.currentState!.validate() && serviceType == "METER CONNECTION") {
-      if(om_1.text.isEmpty || om_2.text.isEmpty || om_3.text.isEmpty || om_4.text.isEmpty || om_5.text.isEmpty) {
-        Notifiers.getToastMessage(context, 'Old Meter Reading is Invalid', 'ERROR');
-      }
-      else if (nm_1.text.isEmpty || nm_2.text.isEmpty || nm_3.text.isEmpty || nm_4.text.isEmpty || nm_5.text.isEmpty){
-        Notifiers.getToastMessage(context, 'New Meter Reading is Invalid', 'ERROR');
-      }
-      else {
-        var oldMeter = om_1.text + om_2.text + om_3.text + om_4.text +
-            om_5.text;
-        var newMeter = nm_1.text + nm_2.text + nm_3.text + nm_4.text +
-            nm_5.text;
-        if (int.parse(oldMeter) < int.parse(newMeter)) {
-          Navigator.of(context).pushReplacement(
-              new MaterialPageRoute(builder: (BuildContext context) {
-                return CommonSuccess(SuccessHandler(
-                    i18.demandGenerate.GENERATE_BILL_SUCCESS,
-                    i18.demandGenerate.GENERATE_BILL_SUCCESS_SUBTEXT,
-                    i18.common.BACK_HOME));
-              }));
-        }
-        else {
-          Notifiers.getToastMessage(
-              context, 'New Meter reading should be greater than old meter reading', 'ERROR');
-        }
-      }
-    }
-    else if(formKey.currentState!.validate() && serviceType == "NON METER CONNECTION")
-      {
-        Navigator.of(context).pushReplacement(
-            new MaterialPageRoute(builder: (BuildContext context) {
-              return CommonSuccess(SuccessHandler(
-                  i18.demandGenerate.GENERATE_BILL_SUCCESS,
-                  i18.demandGenerate.GENERATE_BILL_SUCCESS_SUBTEXT, i18.common.BACK_HOME));
-            }));
-      }
-    else {
-      autoValidation = true;
-    }
-  }
   saveInput(context) async {
     setState(() {
       metVal = context;
     });
   }
-  void onChangeOfMeterType(value) {
-    print(value.toString());
-    setState(() {
-      serviceType = value;
-    });
-  }
-  void onChangeOfServiceCat(value) {
-    print(value.toString());
-    setState(() {
-      serviceCat = value;
-    });
-  }
-  void onChangeOfProperty(value) {
-    print(value.toString());
-    setState(() {
-      propertyType = value;
-    });
-  }
-  void onChangeOfBillYear(value) {
-    print(value.toString());
-    setState(() {
-      billYear = value;
-    });
-  }
-  void onChangeOfBillCycle(value) {
-    print(value.toString());
-    setState(() {
-      billCycle = value;
-    });
-  }
 
-  Widget buildview() {
+  Widget buildview(BillGenerationDetails billGenerationDetails) {
     return Container(
         child: FormWrapper(Column(
             mainAxisAlignment: MainAxisAlignment.start,
@@ -136,106 +45,96 @@ class _GenerateBillState extends State<GenerateBill> {
               Container(
                   height: 1260,
                   width: MediaQuery.of(context).size.width,
-                child: Card(
-                  child: Form(
-                      key: formKey,
-                      autovalidateMode: autoValidation
-                          ? AutovalidateMode.always
-                          : AutovalidateMode.disabled,
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            LabelText(i18.demandGenerate.GENERATE_BILL_HEADER),
-                            SelectFieldBuilder(
-                              i18.demandGenerate.SERVICE_CATEGORY_LABEL,
-                              serviceCat,
-                              '',
-                              '',
-                              onChangeOfServiceCat,
-                              getDropDownList(Constants.SERVICECATEGORY),
-                              true,
-                            ),
-                            SelectFieldBuilder(
-                                i18.demandGenerate.PROPERTY_TYPE_LABEL,
-                                propertyType,
-                                '',
-                                '',
-                                onChangeOfProperty,
-                                getDropDownList(Constants.PROPERTYTYPE),
-                                true),
-                            SelectFieldBuilder(
-                                i18.demandGenerate.SERVICE_TYPE_LABEL,
-                                serviceType,
-                                '',
-                                '',
-                                onChangeOfMeterType,
-                                getDropDownList(Constants.SERVICETYPE),
-                                true),
-                            serviceType != "METER CONNECTION"
-                                ? Container()
-                                : Container(
-                                width: MediaQuery.of(context).size.width,
-                                child: Column(
-                                children: [
-                                  BuildTextField(
-                                    i18.demandGenerate.METER_NUMBER_LABEL,
-                                    meterNumber,
-                                    isRequired: true,
-                                    validator: Validators.meterNumberValidator,
-                                    textInputType: TextInputType.number,
-                                    isDisabled: true,
-                                    onChange: (value) => saveInput(value),
-                                  ),
-                                  MeterReading(i18.demandGenerate
-                                      .PREV_METER_READING_LABEL, om_1, om_2,
-                                      om_3, om_4, om_5),
-                                  MeterReading(i18.demandGenerate
-                                      .NEW_METER_READING_LABEL, nm_1, nm_2,
-                                      nm_3, nm_4, nm_5),
-                                ])),
-                            serviceType != "NON METER CONNECTION"
-                                ? Container()
-                                : Container(
-                                width: MediaQuery.of(context).size.width,
-                                  child : Column(
+                  child: Card(
+                      child: Consumer<BillGenerationProvider>(
+                          builder: (_, billgenerationprovider, child) => Form(
+                              key: billgenerationprovider.formKey,
+                              autovalidateMode: billgenerationprovider.autoValidation
+                                  ? AutovalidateMode.always
+                                  : AutovalidateMode.disabled,
+                              child: Column(
                                   mainAxisAlignment: MainAxisAlignment.start,
                                   children: [
+                                    LabelText(i18.demandGenerate.GENERATE_BILL_HEADER),
                                     SelectFieldBuilder(
-                                      i18.demandGenerate.BILLING_YEAR_LABEL,
-                                      billYear,
+                                      i18.demandGenerate.SERVICE_CATEGORY_LABEL,
+                                      billGenerationDetails.serviceCat,
                                       '',
                                       '',
-                                      onChangeOfBillYear,
-                                      getDropDownList(Constants.BILLINGYEAR),
-                                      true),
+                                      billgenerationprovider.onChangeOfServiceCat,
+                                      billgenerationprovider.getDropDownList(Constants.SERVICECATEGORY),
+                                      true,
+                                    ),
                                     SelectFieldBuilder(
-                                      i18.demandGenerate.BILLING_CYCLE_LABEL,
-                                      billCycle,
-                                      '',
-                                      '',
-                                      onChangeOfBillCycle,
-                                      getDropDownList(Constants.BILLINGCYCLE),
-                                      true),
-                                ])),
-                          ]))))
-            ])));
-  }
-
-
-  List<DropdownMenuItem<Object>> getDropDownList(List<KeyValue>? list) {
-    if (list?.length != 0) {
-      return (list ?? <KeyValue>[]).map((value) {
-        return DropdownMenuItem(
-          value: value.key,
-          child: new Text(value.label),
-        );
-      }).toList();
-    }
-    return <DropdownMenuItem<Object>>[];
+                                        i18.demandGenerate.PROPERTY_TYPE_LABEL,
+                                        billGenerationDetails.propertyType,
+                                        '',
+                                        '',
+                                        billgenerationprovider.onChangeOfProperty,
+                                        billgenerationprovider.getDropDownList(Constants.PROPERTYTYPE),
+                                        true),
+                                    SelectFieldBuilder(
+                                        i18.demandGenerate.SERVICE_TYPE_LABEL,
+                                        billGenerationDetails.serviceType,
+                                        '',
+                                        '',
+                                        billgenerationprovider.onChangeOfServiceType,
+                                        billgenerationprovider.getDropDownList(Constants.SERVICETYPE),
+                                        true),
+                                    billGenerationDetails.serviceType != "METER CONNECTION"
+                                        ? Container()
+                                        : Container(
+                                        width: MediaQuery.of(context).size.width,
+                                        child: Column(
+                                            children: [
+                                              BuildTextField(
+                                                i18.demandGenerate.METER_NUMBER_LABEL,
+                                                billGenerationDetails.meterNumberCtrl,
+                                                isRequired: true,
+                                                validator: Validators.meterNumberValidator,
+                                                textInputType: TextInputType.number,
+                                                isDisabled: true,
+                                                onChange: (value) => saveInput(value),
+                                              ),
+                                              MeterReading(i18.demandGenerate
+                                                  .PREV_METER_READING_LABEL, billGenerationDetails.om_1Ctrl, billGenerationDetails.om_2Ctrl,
+                                                  billGenerationDetails.om_3Ctrl, billGenerationDetails.om_4Ctrl, billGenerationDetails.om_5Ctrl),
+                                              MeterReading(i18.demandGenerate
+                                                  .NEW_METER_READING_LABEL, billGenerationDetails.nm_1Ctrl, billGenerationDetails.nm_2Ctrl,
+                                                  billGenerationDetails.nm_3Ctrl, billGenerationDetails.nm_4Ctrl, billGenerationDetails.nm_5Ctrl),
+                                            ])),
+                                    billGenerationDetails.serviceType != "NON METER CONNECTION"
+                                        ? Container()
+                                        : Container(
+                                        width: MediaQuery.of(context).size.width,
+                                        child : Column(
+                                            mainAxisAlignment: MainAxisAlignment.start,
+                                            children: [
+                                              SelectFieldBuilder(
+                                                  i18.demandGenerate.BILLING_YEAR_LABEL,
+                                                  billGenerationDetails.billYear,
+                                                  '',
+                                                  '',
+                                                  billgenerationprovider.onChangeOfBillYear,
+                                                  billgenerationprovider.getDropDownList(Constants.BILLINGYEAR),
+                                                  true),
+                                              SelectFieldBuilder(
+                                                  i18.demandGenerate.BILLING_CYCLE_LABEL,
+                                                  billGenerationDetails.billCycle,
+                                                  '',
+                                                  '',
+                                                  billgenerationprovider.onChangeOfBillCycle,
+                                                  billgenerationprovider.getDropDownList(Constants.BILLINGCYCLE),
+                                                  true),
+                                            ])),
+                                  ]))))
+              )])));
   }
 
   @override
   Widget build(BuildContext context) {
+    var billgenerateProvider =
+    Provider.of<BillGenerationProvider>(context, listen: false);
     return Scaffold(
         appBar: BaseAppBar(
           Text(i18.common.MGRAM_SEVA),
@@ -246,8 +145,8 @@ class _GenerateBillState extends State<GenerateBill> {
           Drawer(child: SideBar()),
         ),
         body: SingleChildScrollView(
-            child: buildview()),
-        bottomNavigationBar: BottomButtonBar(i18.demandGenerate.GENERATE_BILL_BUTTON, () => _onSubmit(context))
+            child: buildview(billgenerateProvider.billGenerateDetails)),
+        bottomNavigationBar: BottomButtonBar(i18.demandGenerate.GENERATE_BILL_BUTTON, () => {billgenerateProvider.onSubmit(context)})
     );
   }
 }
