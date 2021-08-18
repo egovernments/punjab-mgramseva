7012acac37782758ff90f183b560476958545fad
Darshan J <naveen.j@egovernments.org>
2021-08-19 02:28:15 +0530

    IFIX-34

--- frontend/mgramseva/lib/components/HouseConnectionandBill/GenerateNewBill.dart
+++ frontend/mgramseva/lib/components/HouseConnectionandBill/GenerateNewBill.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mgramseva/model/bill/billing.dart';
 import 'package:mgramseva/routers/Routers.dart';
 import 'package:mgramseva/utils/Constants/I18KeyConstants.dart';
+import 'package:mgramseva/utils/Locilization/application_localizations.dart';
 import 'package:mgramseva/utils/date_formats.dart';
 import 'package:mgramseva/widgets/ListLabelText.dart';
 import 'package:mgramseva/widgets/ShortButton.dart';
@@ -13,19 +14,19 @@ class GenerateNewBill extends StatelessWidget {
 
   _getLabeltext(label, value, context) {
     return (Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
             padding: EdgeInsets.only(top: 16, bottom: 16),
             width: MediaQuery.of(context).size.width / 3,
-            child: Text(
-              label,
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+            child: Padding(
+              padding: EdgeInsets.only(right: 10),
+              child: Text(
+                ApplicationLocalizations.of(context).translate(label),
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+              ),
             )),
-        Container(
-            padding: EdgeInsets.only(top: 16, bottom: 16),
-            child: Text(value,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400)))
+        Text(ApplicationLocalizations.of(context).translate(value),
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400))
       ],
     ));
   }
@@ -56,15 +57,17 @@ class GenerateNewBill extends StatelessWidget {
                                 .bill!.first.meterReadings!.last.currentReading
                                 .toString(),
                         context),
-                    _getLabeltext(i18.generateBillDetails.PENDING_AMOUNT,
-                        billList!.bill!.first.totalAmount.toString(), context),
+                    _getLabeltext(
+                        i18.generateBillDetails.PENDING_AMOUNT,
+                        ('₹' + billList!.bill!.first.totalAmount.toString()),
+                        context),
                     ShortButton(
                         i18.generateBillDetails.GENERATE_NEW_BTN_LABEL,
-                            () => {
-                          Navigator.pushNamed(context, Routes.BILL_GENERATE,
-                              arguments:
-                              billList!.bill!.first.waterConnection)
-                        })
+                        () => {
+                              Navigator.pushNamed(context, Routes.BILL_GENERATE,
+                                  arguments:
+                                      billList!.bill!.first.waterConnection)
+                            })
                   ],
                 )))
       ],
--- frontend/mgramseva/lib/services/urls.dart
+++ frontend/mgramseva/lib/services/urls.dart
@@ -40,6 +40,9 @@ class Url {
 
   /// Connection bill payment
   static const String COLLECT_PAYMENT = 'collection-services/payments/_create';
+
+  static const String FETCH_BILL_PAYMENTS =
+      'collection-services/payments/_search';
 }
 
 class UserUrl {
