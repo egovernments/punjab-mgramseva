66ded0f6f01a3a500dcc469f967a28f87df699f5
narendrabandhamneni-wt-egov <narendra.bandhamaneni@egovernments.org>
2021-08-30 20:12:56 +0530

    IFIX-279  IFIX Adapter integration

---
+++ municipal-services/mgramseva-ifix-adapter/src/main/java/org/egov/mgramsevaifixadaptor/util/Constants.java
@@ -0,0 +1,19 @@
+package org.egov.mgramsevaifixadaptor.util;
+
+import com.jayway.jsonpath.Configuration;
+
+public class Constants {
+
+	public static final String DEMAND="DEMAND";
+	
+	public static final String RECEIPT="RECEIPT";
+	
+	public static final String BILL="BILL";
+
+	public static final String TENANT = "tenant";
+
+	public static final String PROJECTMODULE = "projectmodule";
+
+	public static final String PROJECT_JSON_PATH = "$.[0].code";
+	
+}
