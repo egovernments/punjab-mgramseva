781f4bc38c6518e9b56c0da104b9befae5ffa117
naveen-egov <naveen.j@egovernments.org>
2021-09-02 14:46:23 +0530

    IFIX-25

--- frontend/mgramseva/lib/widgets/Notifications.dart
+++ frontend/mgramseva/lib/widgets/Notifications.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mgramseva/model/Events/events_List.dart';
+import 'package:mgramseva/routers/Routers.dart';
 import 'package:mgramseva/utils/Constants/I18KeyConstants.dart';
 import 'package:mgramseva/utils/Locilization/application_localizations.dart';
 
@@ -29,58 +30,66 @@ class _NotificationsState extends State<Notifications> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                  child: Padding(
-                      padding: EdgeInsets.all(8),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                              padding: EdgeInsets.all(4),
-                              child: Text(
-                                ApplicationLocalizations.of(context)
-                                    .translate(widget.event!.name!),
-                                textAlign: TextAlign.left,
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w700),
-                              )),
-                          Padding(
-                              padding: EdgeInsets.all(4),
-                              child: Text(
-                                ApplicationLocalizations.of(context)
-                                    .translate(widget.event!.description!),
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w400),
-                                textAlign: TextAlign.left,
-                              )),
-                          Padding(
-                              padding: EdgeInsets.only(
-                                  top: 24, bottom: 4, left: 4, right: 4),
-                              child: Text(
-                                DateTime.now()
-                                        .difference(
-                                            DateTime.fromMillisecondsSinceEpoch(
-                                                widget.event!.auditDetails!
-                                                    .createdTime!))
-                                        .inDays
-                                        .toString() +
-                                    " " +
-                                    ApplicationLocalizations.of(context)
-                                        .translate(
-                                            i18.generateBillDetails.DAYS_AGO),
-                                style: TextStyle(
-                                    color: Theme.of(context).primaryColor),
-                              ))
-                        ],
-                      ))),
-              Align(
-                  alignment: Alignment.topRight,
+              GestureDetector(
+                  onTap: () {
+                    widget.event!.actions != null
+                        ? Navigator.pushNamed(context, widget.event!.actions!)
+                        : null;
+                  },
                   child: Container(
-                      child: IconButton(
-                    icon: Icon(Icons.close),
-                    onPressed: null,
-                  ))),
+                      child: Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                  padding: EdgeInsets.all(4),
+                                  child: Text(
+                                    ApplicationLocalizations.of(context)
+                                        .translate(widget.event!.name!),
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w700),
+                                  )),
+                              Padding(
+                                  padding: EdgeInsets.all(4),
+                                  child: Text(
+                                    ApplicationLocalizations.of(context)
+                                        .translate(widget.event!.description!),
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w400),
+                                    textAlign: TextAlign.left,
+                                  )),
+                              Padding(
+                                  padding: EdgeInsets.only(
+                                      top: 24, bottom: 4, left: 4, right: 4),
+                                  child: Text(
+                                    DateTime.now()
+                                            .difference(DateTime
+                                                .fromMillisecondsSinceEpoch(
+                                                    widget.event!.auditDetails!
+                                                        .createdTime!))
+                                            .inDays
+                                            .toString() +
+                                        " " +
+                                        ApplicationLocalizations.of(context)
+                                            .translate(i18
+                                                .generateBillDetails.DAYS_AGO),
+                                    style: TextStyle(
+                                        color: Theme.of(context).primaryColor),
+                                  ))
+                            ],
+                          )))),
+              // Align(
+              //     alignment: Alignment.topRight,
+              //     child: Container(
+              //         child: IconButton(
+              //       icon: Icon(Icons.close),
+              //       onPressed: null,
+              //     ))),
             ],
           )),
     );
