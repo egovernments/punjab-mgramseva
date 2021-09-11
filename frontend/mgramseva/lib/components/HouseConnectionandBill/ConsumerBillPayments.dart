88758c90a1957fee7f9b677c8a98304041b720f5
naveen-egov <naveen.j@egovernments.org>
2021-09-11 16:06:56 +0530

    IFIX-34

--- frontend/mgramseva/lib/components/HouseConnectionandBill/ConsumerBillPayments.dart
+++ frontend/mgramseva/lib/components/HouseConnectionandBill/ConsumerBillPayments.dart
@@ -55,109 +55,117 @@ class ConsumerBillPaymentsState extends State<ConsumerBillPayments> {
   buildBillPaymentsView(BillPayments billpayments) {
     var commonProvider = Provider.of<CommonProvider>(context, listen: false);
     return LayoutBuilder(builder: (context, constraints) {
-      return Column(children: [
-        billpayments.payments!.length > 0
-            ? ListLabelText(i18.consumerReciepts.CONSUMER_BILL_RECIEPTS_LABEL)
-            : Text(""),
-        for (var item in billpayments.payments!)
-          Card(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                  padding: EdgeInsets.all(8),
+      return Container(
+          padding: EdgeInsets.only(top: 16, bottom: 16),
+          child: Column(children: [
+            billpayments.payments!.length > 0
+                ? ListLabelText(
+                    i18.consumerReciepts.CONSUMER_BILL_RECIEPTS_LABEL)
+                : Text(""),
+            for (var item in billpayments.payments!)
+              Card(
                   child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Visibility(
-                          visible: true,
-                          child: TextButton.icon(
-                            onPressed: () =>
-                                commonProvider.getFileFromPDFPaymentService({
-                              "Payments": [item]
-                            }, {
-                              "key": widget.waterconnection?.connectionType ==
-                                      'Metered'
-                                  ? "ws-receipt"
-                                  : "ws-receipt-nm",
-                              "tenantId": commonProvider
-                                  .userDetails!.selectedtenant!.code,
-                            }, item.mobileNumber, item, "Download"),
-                            icon: Icon(Icons.download_sharp),
-                            label: Text(
-                                ApplicationLocalizations.of(context)
-                                    .translate(i18.common.RECEIPT_DOWNLOAD),
-                                style: TextStyle(fontSize: 16)),
-                          ),
-                        ),
-                        _getLabeltext(
-                            i18.consumerReciepts.CONSUMER_BILL_RECIEPT_ID,
-                            (item.paymentDetails!.first.receiptNumber)
-                                .toString(),
-                            context),
-                        _getLabeltext(
-                            i18.consumerReciepts.CONSUMER_RECIEPT_PAID_AMOUNT,
-                            ('₹' + (item.totalAmountPaid).toString()),
-                            context),
-                        _getLabeltext(
-                            i18.consumerReciepts.CONSUMER_RECIEPT_PAID_DATE,
-                            DateFormats.timeStampToDate(item.transactionDate,
-                                    format: "dd-MM-yyyy")
-                                .toString(),
-                            context),
-                        Row(children: [
-                          Container(
-                            width: constraints.maxWidth > 760
-                                ? MediaQuery.of(context).size.width / 3
-                                : MediaQuery.of(context).size.width / 1.17,
-                            child: OutlinedButton.icon(
-                              onPressed: () =>
-                                  commonProvider.getFileFromPDFPaymentService({
-                                "Payments": [item]
-                              }, {
-                                "key": widget.waterconnection?.connectionType ==
-                                        'Metered'
-                                    ? "ws-receipt"
-                                    : "ws-receipt-nm",
-                                "tenantId": commonProvider
-                                    .userDetails!.selectedtenant!.code,
-                              }, item.mobileNumber, item, "Share"),
-                              style: ButtonStyle(
-                                alignment: Alignment.center,
-                                padding: MaterialStateProperty.all(
-                                    EdgeInsets.symmetric(vertical: 8)),
-                                shape: MaterialStateProperty.all(
-                                    RoundedRectangleBorder(
-                                  side: BorderSide(
-                                      width: 2,
-                                      color: Theme.of(context).primaryColor),
-                                  borderRadius: BorderRadius.circular(0.0),
-                                )),
-                              ),
-                              icon: (Image.asset('assets/png/whats_app.png')),
-                              label: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 5),
-                                child: Text(
-                                  ApplicationLocalizations.of(context)
-                                      .translate(i18.consumerReciepts
-                                          .CONSUMER_RECIEPT_SHARE_RECEIPT),
-                                  style: Theme.of(context).textTheme.subtitle2,
-                                ),
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Visibility(
+                              visible: true,
+                              child: TextButton.icon(
+                                onPressed: () => commonProvider
+                                    .getFileFromPDFPaymentService({
+                                  "Payments": [item]
+                                }, {
+                                  "key":
+                                      widget.waterconnection?.connectionType ==
+                                              'Metered'
+                                          ? "ws-receipt"
+                                          : "ws-receipt-nm",
+                                  "tenantId": commonProvider
+                                      .userDetails!.selectedtenant!.code,
+                                }, item.mobileNumber, item, "Download"),
+                                icon: Icon(Icons.download_sharp),
+                                label: Text(
+                                    ApplicationLocalizations.of(context)
+                                        .translate(i18.common.RECEIPT_DOWNLOAD),
+                                    style: TextStyle(fontSize: 16)),
                               ),
                             ),
-                          )
-                        ])
-                      ]))
-            ],
-          ))
-      ]);
+                            _getLabeltext(
+                                i18.consumerReciepts.CONSUMER_BILL_RECIEPT_ID,
+                                (item.paymentDetails!.first.receiptNumber)
+                                    .toString(),
+                                context),
+                            _getLabeltext(
+                                i18.consumerReciepts
+                                    .CONSUMER_RECIEPT_PAID_AMOUNT,
+                                ('₹' + (item.totalAmountPaid).toString()),
+                                context),
+                            _getLabeltext(
+                                i18.consumerReciepts.CONSUMER_RECIEPT_PAID_DATE,
+                                DateFormats.timeStampToDate(
+                                        item.transactionDate,
+                                        format: "dd-MM-yyyy")
+                                    .toString(),
+                                context),
+                            Row(children: [
+                              Container(
+                                width: constraints.maxWidth > 760
+                                    ? MediaQuery.of(context).size.width / 3
+                                    : MediaQuery.of(context).size.width / 1.11,
+                                child: OutlinedButton.icon(
+                                  onPressed: () => commonProvider
+                                      .getFileFromPDFPaymentService({
+                                    "Payments": [item]
+                                  }, {
+                                    "key": widget.waterconnection
+                                                ?.connectionType ==
+                                            'Metered'
+                                        ? "ws-receipt"
+                                        : "ws-receipt-nm",
+                                    "tenantId": commonProvider
+                                        .userDetails!.selectedtenant!.code,
+                                  }, item.mobileNumber, item, "Share"),
+                                  style: ButtonStyle(
+                                    alignment: Alignment.center,
+                                    padding: MaterialStateProperty.all(
+                                        EdgeInsets.symmetric(vertical: 8)),
+                                    shape: MaterialStateProperty.all(
+                                        RoundedRectangleBorder(
+                                      side: BorderSide(
+                                          width: 2,
+                                          color:
+                                              Theme.of(context).primaryColor),
+                                      borderRadius: BorderRadius.circular(0.0),
+                                    )),
+                                  ),
+                                  icon:
+                                      (Image.asset('assets/png/whats_app.png')),
+                                  label: Padding(
+                                    padding:
+                                        const EdgeInsets.symmetric(vertical: 5),
+                                    child: Text(
+                                      ApplicationLocalizations.of(context)
+                                          .translate(i18.consumerReciepts
+                                              .CONSUMER_RECIEPT_SHARE_RECEIPT),
+                                      style:
+                                          Theme.of(context).textTheme.subtitle2,
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ])
+                          ]))
+                ],
+              ))
+          ]));
     });
   }
-
-  //  Text(billpayments.payments!.first.totalDue.toString());
 
   @override
   Widget build(BuildContext context) {
