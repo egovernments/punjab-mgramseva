8e5d639f02dcf99f89f89c317989be0a6d8dd8c2
naveen-egov <naveen.j@egovernments.org>
2021-09-15 01:07:33 +0530

    IFIX-495

--- frontend/mgramseva/lib/screeens/Home/Home.dart
+++ frontend/mgramseva/lib/screeens/Home/Home.dart
@@ -170,7 +170,7 @@ class _HomeState extends State<Home> {
                 });
             }
             return userProvider.userDetails?.selectedtenant?.code != null
-                ? Text("")
+                ? NotificationsList()
                 : Text("");
           })),
     );
