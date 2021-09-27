7006b45c0159047c803a5efaab2867893543ecbc
lakshmisravani-wtt-egov <80089327+lakshmisravani-wtt-egov@users.noreply.github.com>
2021-09-27 15:09:39 +0530

    made changes and added logs.

--- municipal-services/ws-calculator/src/main/java/org/egov/wscalculation/service/DemandService.java
+++ municipal-services/ws-calculator/src/main/java/org/egov/wscalculation/service/DemandService.java
@@ -267,8 +267,7 @@ public class DemandService {
 				
 				if(billNumber.size() > 0 ) {
 					actionLink = actionLink.replace("$billNumber", billNumber.get(0));		
-				}
-				actionLink = actionLink.replace("$billNumber", billNumber.get(0));		
+				}		
 				billCycle = (Instant.ofEpochMilli(fromDate).atZone(ZoneId.systemDefault()).toLocalDate() + "-"
 						+ Instant.ofEpochMilli(toDate).atZone(ZoneId.systemDefault()).toLocalDate());
 				messageString = messageString.replace("{ownername}", owner.getName());
@@ -1059,7 +1058,7 @@ public class DemandService {
 						calculatorUtils.getFetchBillURL(demand.getTenantId(), demand.getConsumerCode()),
 						RequestInfoWrapper.builder().requestInfo(requestInfo).build());
 				billNumber = JsonPath.read(result, "$.Bill.*.billNumber");
-				
+				log.info("Bill Response :: " + result);
 
 				HashMap<String, Object> billResponse = new HashMap<>();
 				
