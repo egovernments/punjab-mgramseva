c500e4a82de46da0f688887f7e54867d50e94da4
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2023-01-25 17:46:36 +0530

    Bihar Configs added

--- frontend/mgramseva/lib/services/MDMS.dart
+++ frontend/mgramseva/lib/services/MDMS.dart
@@ -1,13 +1,3 @@
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:http/http.dart' as http;
-import 'package:mgramseva/Env/app_config.dart';
-import 'package:mgramseva/services/RequestInfo.dart';
-import 'package:mgramseva/services/urls.dart';
-import 'package:mgramseva/utils/global_variables.dart';
-
 initRequestBody(tenantId) {
   return {
     "MdmsCriteria": {
@@ -155,41 +145,4 @@ Map getMdmsPaymentModes(String tenantId) {
       ]
     }
   };
-}
-
-Future getMDMD() async {
-  final requestInfo = RequestInfo(
-      APIConstants.API_MODULE_NAME,
-      APIConstants.API_VERSION,
-      APIConstants.API_TS,
-      "_search",
-      APIConstants.API_DID,
-      APIConstants.API_KEY,
-      APIConstants.API_MESSAGE_ID,
-      "");
-  var response = await http.post(Uri.parse(apiBaseUrl.toString() + Url.MDMS),
-      headers: {
-        HttpHeaders.contentTypeHeader: 'application/json',
-      },
-      body: json.encode({
-        "RequestInfo": requestInfo.toJson(),
-        ...initRequestBody({"tenantId": "pb"})
-      }));
-
-  print('Response status: ${response.statusCode}');
-
-  if (response.statusCode == 200) {
-// Write value
-    if (kIsWeb) {
-      // Use flutter_secure_storage
-      // await storage.write(
-      //     key: 'token', value: json.decode(response.body)['token']);
-      // } else {
-      // Use localStorage - unsafe
-      // storage.setItem("local", json.decode(response.body)['messages'].toString());
-      // window.localStorage['local'] = ;
-    }
-  }
-
-  return (response);
 }
