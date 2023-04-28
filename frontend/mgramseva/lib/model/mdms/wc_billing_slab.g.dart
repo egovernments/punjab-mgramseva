73253aa9277fe04df406f79e0abe08dc78d97cec
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2023-04-28 11:59:51 +0530

    PFM-1730:: PR Suggestion changes

---
+++ frontend/mgramseva/lib/model/mdms/wc_billing_slab.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'wc_billing_slab.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+WCBillingSlab _$WCBillingSlabFromJson(Map<String, dynamic> json) =>
+    WCBillingSlab()
+      ..id = json['id'] as String?
+      ..buildingType = json['buildingType'] as String?
+      ..connectionType = json['connectionType'] as String?
+      ..calculationAttribute = json['calculationAttribute'] as String?
+      ..minimumCharge = json['minimumCharge'] as String?
+      ..slabs = (json['slabs'] as List<dynamic>?)
+          ?.map((e) => Slabs.fromJson(e as Map<String, dynamic>))
+          .toList();
+
+Map<String, dynamic> _$WCBillingSlabToJson(WCBillingSlab instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'buildingType': instance.buildingType,
+      'connectionType': instance.connectionType,
+      'calculationAttribute': instance.calculationAttribute,
+      'minimumCharge': instance.minimumCharge,
+      'slabs': instance.slabs,
+    };
+
+Slabs _$SlabsFromJson(Map<String, dynamic> json) => Slabs()
+  ..from = json['from'] as String?
+  ..to = json['to'] as String?
+  ..charge = json['charge'] as String?
+  ..meterCharge = json['meterCharge'] as String?;
+
+Map<String, dynamic> _$SlabsToJson(Slabs instance) => <String, dynamic>{
+      'from': instance.from,
+      'to': instance.to,
+      'charge': instance.charge,
+      'meterCharge': instance.meterCharge,
+    };
