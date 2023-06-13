967c4d8235716c32897e20b4af7709238bc7cf08
Rahul Dev Garg <34365102+rahuldevgarg@users.noreply.github.com>
2023-06-13 13:11:55 +0530

    import fix

--- frontend/mgramseva/lib/screeens/add_expense/add_expense_walk_through/walk_through_container.dart
+++ frontend/mgramseva/lib/screeens/add_expense/add_expense_walk_through/walk_through_container.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mgramseva/providers/common_provider.dart';
 import 'package:mgramseva/providers/expenses_details_provider.dart';
-import 'package:mgramseva/screeens/consumer_details//pointer.dart';
+import 'package:mgramseva/screeens/consumer_details/pointer.dart';
 import 'package:mgramseva/utils/constants/i18_key_constants.dart';
 import 'package:mgramseva/utils/localization/application_localizations.dart';
 import 'package:mgramseva/utils/constants.dart';
