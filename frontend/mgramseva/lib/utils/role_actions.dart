c78fcfa921ddd177f8f93dc9172f7c0d78d59f4c
Rahul Dev Garg <34365102+rahuldevgarg@users.noreply.github.com>
2023-09-04 15:47:19 +0530

    added roles for report

--- frontend/mgramseva/lib/utils/role_actions.dart
+++ frontend/mgramseva/lib/utils/role_actions.dart
@@ -1,4 +1,3 @@
-
 import 'package:mgramseva/providers/common_provider.dart';
 import 'package:mgramseva/routers/routers.dart';
 import 'package:mgramseva/utils/constants.dart';
@@ -21,8 +20,8 @@ class RoleActionsFiltering {
       getRolesBasedOnModule(item.link).forEach((element) {
         var roles = commonProvider.userDetails?.userRequest?.roles
             ?.where((e) =>
-                e.code == element &&
-                commonProvider.userDetails?.selectedtenant?.code == e.tenantId)
+        e.code == element &&
+            commonProvider.userDetails?.selectedtenant?.code == e.tenantId)
             .toList();
 
         if (roles?.isEmpty ?? true) {
@@ -58,8 +57,8 @@ class RoleActionsFiltering {
       getRolesBasedOnModule(routerLink).forEach((element) {
         var roles = commonProvider.userDetails?.userRequest?.roles
             ?.where((e) =>
-                e.code == element &&
-                commonProvider.userDetails?.selectedtenant?.code == e.tenantId)
+        e.code == element &&
+            commonProvider.userDetails?.selectedtenant?.code == e.tenantId)
             .toList();
         if (roles?.isEmpty ?? true) {
           isEligible = false;
@@ -87,56 +86,57 @@ class RoleActionsFiltering {
 
   List<String> getRolesBasedOnModule(String route) {
     switch (route) {
-      // GP Admin
+    // GP Admin
       case Routes.HOUSEHOLD:
-        return ['COLLECTION_OPERATOR', 'SUPERUSER'];
+        return ['COLLECTION_OPERATOR', 'SUPERUSER', 'DIV_ADMIN'];
 
       case Routes.CONSUMER_UPDATE:
-        return ['GP_ADMIN', 'SUPERUSER', 'COLLECTION_OPERATOR'];
+        return ['GP_ADMIN', 'SUPERUSER', 'COLLECTION_OPERATOR', 'DIV_ADMIN'];
 
       case Routes.CONSUMER_SEARCH_UPDATE:
-        return ['GP_ADMIN', 'SUPERUSER', 'COLLECTION_OPERATOR'];
+        return ['GP_ADMIN', 'SUPERUSER', 'COLLECTION_OPERATOR', 'DIV_ADMIN'];
 
       case Routes.CONSUMER_CREATE:
-        return ['GP_ADMIN', 'SUPERUSER', 'COLLECTION_OPERATOR'];
+        return ['GP_ADMIN', 'SUPERUSER', 'COLLECTION_OPERATOR', 'DIV_ADMIN'];
 
       case Routes.HOUSEHOLD_DETAILS:
-        return ['BULK_DEMAND_PROCESSING', 'COLLECTION_OPERATOR', 'SUPERUSER'];
+        return ['BULK_DEMAND_PROCESSING', 'COLLECTION_OPERATOR', 'SUPERUSER', 'DIV_ADMIN'];
 
       case Routes.SEARCH_CONSUMER_RESULT:
         return [
           'GP_ADMIN',
           'BULK_DEMAND_PROCESSING',
           'COLLECTION_OPERATOR',
-          'SUPERUSER'
+          'SUPERUSER',
+          'DIV_ADMIN'
         ];
 
       case Routes.HOUSEHOLD_REGISTER:
-        return ['COLLECTION_OPERATOR', 'SUPERUSER'];
+        return ['COLLECTION_OPERATOR', 'SUPERUSER', 'DIV_ADMIN'];
 
-      // Expense Processing
+    // Expense Processing
       case Routes.EXPENSE_SEARCH:
-        return ['EXPENSE_PROCESSING', 'SUPERUSER'];
+        return ['EXPENSE_PROCESSING', 'SUPERUSER', 'DIV_ADMIN'];
       case Routes.EXPENSES_ADD:
-        return ['EXPENSE_PROCESSING', 'SUPERUSER'];
+        return ['EXPENSE_PROCESSING', 'SUPERUSER', 'DIV_ADMIN'];
       case Routes.EXPENSE_UPDATE:
-        return ['EXPENSE_PROCESSING', 'SUPERUSER'];
+        return ['EXPENSE_PROCESSING', 'SUPERUSER', 'DIV_ADMIN'];
       case Routes.HOUSEHOLDRECEIPTS:
-        return ['BULK_DEMAND_PROCESSING', 'COLLECTION_OPERATOR', 'SUPERUSER'];
+        return ['BULK_DEMAND_PROCESSING', 'COLLECTION_OPERATOR', 'SUPERUSER', 'DIV_ADMIN'];
 
       case Routes.MANUAL_BILL_GENERATE:
-        return ['BULK_DEMAND_PROCESSING', 'SUPERUSER'];
+        return ['BULK_DEMAND_PROCESSING', 'SUPERUSER', 'DIV_ADMIN'];
 
-      // Collection Operator
+    // Collection Operator
       case Routes.CONSUMER_SEARCH:
-        return ['COLLECTION_OPERATOR', 'SUPERUSER'];
+        return ['COLLECTION_OPERATOR', 'SUPERUSER', 'DIV_ADMIN'];
       case Routes.BILL_GENERATE:
-        return ['COLLECTION_OPERATOR', 'SUPERUSER'];
+        return ['COLLECTION_OPERATOR', 'SUPERUSER', 'DIV_ADMIN'];
 
       case Routes.DASHBOARD:
-        return ['SUPERUSER', 'DASHBOARD_VIEWER'];
+        return ['SUPERUSER', 'DASHBOARD_VIEWER', 'GP_ADMIN', 'DIV_ADMIN'];
       case Routes.REPORTS:
-        return ['SUPERUSER', 'DASHBOARD_VIEWER', 'GP_ADMIN'];
+        return ['SUPERUSER', 'DASHBOARD_VIEWER', 'GP_ADMIN', 'DIV_ADMIN'];
       default:
         return [];
     }
