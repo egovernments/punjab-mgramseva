83bf15c3b471304d482a62a0b726a4b050aa0afd
rakesh-wt-egov <rakesh.katagouni@egovernments.org>
2023-09-05 16:33:25 +0530

    changed publicPath to mgramseva-web

--- frontend/micro-ui/web/webpack.config.js
+++ frontend/micro-ui/web/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "build"),
-    publicPath: "/sanitation-ui/",
+    publicPath: "/mgramseva-web/",
   },
   optimization: {
     splitChunks: {
