171b9806f837ab1a7667e2f981b69899520c7c2a
rakesh-wt-egov <rakesh.katagouni@egovernments.org>
2023-09-05 19:00:30 +0530

    updated globalConfig

--- frontend/micro-ui/web/micro-ui-internals/example/src/index.js
+++ frontend/micro-ui/web/micro-ui-internals/example/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom";
 
 import { initLibraries } from "@egovernments/digit-ui-libraries";
 import { initFSMLibraries } from "@egovernments/digit-ui-fsm-libraries";
-import { PaymentModule, PaymentLinks, paymentConfigs } from "@egovernments/digit-ui-module-common";
+import {
+  PaymentModule,
+  PaymentLinks,
+  paymentConfigs,
+} from "@egovernments/digit-ui-module-common";
 import { initDSSComponents } from "@egovernments/digit-ui-module-dss";
 import { initFSMComponents } from "@egovernments/digit-ui-module-fsm";
 import { initEngagementComponents } from "@egovernments/digit-ui-module-engagement";
 import { DigitUI } from "@egovernments/digit-ui-module-core";
-import { HRMSModule, initHRMSComponents } from "@egovernments/digit-ui-module-hrms";
+import {
+  HRMSModule,
+  initHRMSComponents,
+} from "@egovernments/digit-ui-module-hrms";
 import { initUtilitiesComponents } from "@egovernments/digit-ui-module-utilities";
 import { UICustomizations } from "./UICustomizations";
 import "@egovernments/digit-ui-fsm-css/example/index.css";
@@ -26,18 +33,25 @@ const enabledModules = [
 ];
 
 const initTokens = (stateCode) => {
-  const userType = window.sessionStorage.getItem("userType") || process.env.REACT_APP_USER_TYPE || "CITIZEN";
+  const userType =
+    window.sessionStorage.getItem("userType") ||
+    process.env.REACT_APP_USER_TYPE ||
+    "CITIZEN";
 
-  const token = window.localStorage.getItem("token") || process.env[`REACT_APP_${userType}_TOKEN`];
+  const token =
+    window.localStorage.getItem("token") ||
+    process.env[`REACT_APP_${userType}_TOKEN`];
 
   const citizenInfo = window.localStorage.getItem("Citizen.user-info");
 
-  const citizenTenantId = window.localStorage.getItem("Citizen.tenant-id") || stateCode;
+  const citizenTenantId =
+    window.localStorage.getItem("Citizen.tenant-id") || stateCode;
 
   const employeeInfo = window.localStorage.getItem("Employee.user-info");
   const employeeTenantId = window.localStorage.getItem("Employee.tenant-id");
 
-  const userTypeInfo = userType === "CITIZEN" || userType === "QACT" ? "citizen" : "employee";
+  const userTypeInfo =
+    userType === "CITIZEN" || userType === "QACT" ? "citizen" : "employee";
   window.Digit.SessionStorage.set("user_type", userTypeInfo);
   window.Digit.SessionStorage.set("userType", userTypeInfo);
 
@@ -52,11 +66,13 @@ const initTokens = (stateCode) => {
 
   window.Digit.SessionStorage.set("Citizen.tenantId", citizenTenantId);
 
-  if (employeeTenantId && employeeTenantId.length) window.Digit.SessionStorage.set("Employee.tenantId", employeeTenantId);
+  if (employeeTenantId && employeeTenantId.length)
+    window.Digit.SessionStorage.set("Employee.tenantId", employeeTenantId);
 };
 
 const initDigitUI = () => {
-  window.contextPath = window?.globalConfigs?.getConfig("CONTEXT_PATH") || "sanitation-ui";
+  window.contextPath =
+    window?.globalConfigs?.getConfig("CONTEXT_PATH") || "mgramseva-web";
 
   window.Digit.ComponentRegistryService.setupRegistry({
     ...paymentConfigs,
@@ -76,17 +92,26 @@ const initDigitUI = () => {
   window.Digit.Customizations = {
     TL: {
       customiseCreateFormData: (formData, licenceObject) => licenceObject,
-      customiseRenewalCreateFormData: (formData, licenceObject) => licenceObject,
+      customiseRenewalCreateFormData: (formData, licenceObject) =>
+        licenceObject,
       customiseSendbackFormData: (formData, licenceObject) => licenceObject,
     },
     commonUiConfig: UICustomizations,
   };
 
-  const stateCode = window?.globalConfigs?.getConfig("STATE_LEVEL_TENANT_ID") || "pb";
+  const stateCode =
+    window?.globalConfigs?.getConfig("STATE_LEVEL_TENANT_ID") || "pb";
   initTokens(stateCode);
 
   const registry = window?.Digit.ComponentRegistryService.getRegistry();
-  ReactDOM.render(<DigitUI stateCode={stateCode} enabledModules={enabledModules} moduleReducers={moduleReducers} />, document.getElementById("root"));
+  ReactDOM.render(
+    <DigitUI
+      stateCode={stateCode}
+      enabledModules={enabledModules}
+      moduleReducers={moduleReducers}
+    />,
+    document.getElementById("root")
+  );
 };
 
 initLibraries().then(() => {
