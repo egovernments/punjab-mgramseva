9c6932298acf6091c50e65ecce2790666d5ff7d5
Rahul Dev Garg <34365102+rahuldevgarg@users.noreply.github.com>
2023-09-29 12:59:14 +0530

    addded required annotation

--- municipal-services/ws-services/src/main/java/org/egov/waterconnection/repository/rowmapper/InactiveConsumerReportRowMapper.java
+++ municipal-services/ws-services/src/main/java/org/egov/waterconnection/repository/rowmapper/InactiveConsumerReportRowMapper.java
@@ -10,11 +10,12 @@ import org.egov.waterconnection.web.models.users.UserSearchRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
-
+@Component
 public class InactiveConsumerReportRowMapper implements ResultSetExtractor<List<InactiveConsumerReportData>> {
     @Autowired
     private ObjectMapper mapper;
