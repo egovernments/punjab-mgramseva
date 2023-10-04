9748961f0b8fba80941d07ba87eab6a71e7ecc1d
Saloni-eGov <saloni.bajaj@egovernments.org>
2023-10-04 11:42:38 +0530

    PFM-4087 Inactive consumer report.

--- municipal-services/ws-services/src/main/java/org/egov/waterconnection/repository/WaterDaoImpl.java
+++ municipal-services/ws-services/src/main/java/org/egov/waterconnection/repository/WaterDaoImpl.java
@@ -638,7 +638,7 @@ public class WaterDaoImpl implements WaterDao {
 		}
 
 		List<InactiveConsumerReportData> inactiveConsumerReportList=new ArrayList<>();
-		log.info("query :"+ inactive_consumer_query.toString() +"prepared statement "+ preparedStatement.toArray());
+		log.info("Query :"+ inactive_consumer_query.toString() +"prepared statement "+ preparedStatement.toArray());
 		inactiveConsumerReportList=jdbcTemplate.query(inactive_consumer_query.toString(), preparedStatement.toArray(),inactiveConsumerReportRowMapper);
          return inactiveConsumerReportList;
     }
