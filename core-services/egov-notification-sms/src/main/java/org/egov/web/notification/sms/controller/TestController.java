2eeec6c7d89161bb1dd0a72fdaaf4a5cedae0955
debasishchakraborty-egovt <debasish@egovernmets.org>
2023-10-05 14:04:52 +0530

    PFM-4871: added the changes to persist sms in a table in mgramseva

--- core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/controller/TestController.java
+++ core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/controller/TestController.java
@@ -25,7 +25,7 @@ public class TestController {
 		
 	//Sms sms = new Sms(number, sms, Category.OTP, expirytime);
 		
-		Sms sms = new Sms(number, msg, category, expirytime, "123");
+		Sms sms = new Sms(number, msg, category, expirytime, "123","pb");
 		
 			smsService.sendSMS(sms);
 		
---
+++ core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/models/SmsSaveRequest.java
@@ -0,0 +1,25 @@
+package org.egov.web.notification.sms.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class SmsSaveRequest {
+    private String mobileNumber;
+    private String message;
+    private Category category;
+    private Long createdtime;
+    private String templateId;
+    private String tenantId;
+    public boolean isValid() {
+
+        return isNotEmpty(mobileNumber) && isNotEmpty(message);
+    }
+}
