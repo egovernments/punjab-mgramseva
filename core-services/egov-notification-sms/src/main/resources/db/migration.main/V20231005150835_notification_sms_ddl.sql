1ccdb4ee911915c0763e8958b56895e478cb1f47
debasishchakraborty-egovt <debasish@egovernmets.org>
2023-10-05 16:49:40 +0530

    PFM-4871: added the changes to persist sms in a table in mgramseva

--- core-services/egov-notification-sms/src/main/resources/db/migration.main/V20231005150835_notification_sms_ddl.sql
+++ core-services/egov-notification-sms/src/main/resources/db/migration.main/V20231005150835_notification_sms_ddl.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS  eg_notification_sms (
-    id CHARACTER VARYING (128) NOT NULL,
+    id SERIAL PRIMARY KEY,
     mobile_no VARCHAR(20) NOT NULL,
     message TEXT,
     category VARCHAR(50),
