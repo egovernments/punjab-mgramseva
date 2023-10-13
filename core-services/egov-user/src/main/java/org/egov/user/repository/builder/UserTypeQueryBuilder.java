5f4efbe5413368411f8f99b35e40ad80c2b35775
debasishchakraborty-egovt <debasish@egovernmets.org>
2023-10-13 15:52:02 +0530

    PFM-1419-User search  : added change to Employee search based on user role tenantid

--- core-services/egov-user/src/main/java/org/egov/user/repository/builder/UserTypeQueryBuilder.java
+++ core-services/egov-user/src/main/java/org/egov/user/repository/builder/UserTypeQueryBuilder.java
@@ -242,14 +242,20 @@ public class UserTypeQueryBuilder {
         boolean isAppendAndClause = false;
 
         if (userSearchCriteria.getTenantId() != null ) {
-            if(ObjectUtils.isEmpty(userSearchCriteria.getIsStateLevelSearch())){
+            if(ObjectUtils.isEmpty(userSearchCriteria.getIsStateLevelSearch()) ){
                 isAppendAndClause = addAndClauseIfRequired(false, selectQuery);
                 selectQuery.append(" ur.role_tenantid like ? ");
                 preparedStatementValues.add( '%' +  userSearchCriteria.getTenantId().trim() + '%');
             } else {
-                isAppendAndClause = addAndClauseIfRequired(false, selectQuery);
-                selectQuery.append(" ur.role_tenantid = ?");
-                preparedStatementValues.add(userSearchCriteria.getTenantId());
+                if(!userSearchCriteria.getIsStateLevelSearch()) {
+                    isAppendAndClause = addAndClauseIfRequired(false, selectQuery);
+                    selectQuery.append(" ur.role_tenantid = ?");
+                    preparedStatementValues.add(userSearchCriteria.getTenantId());
+                } else {
+                    isAppendAndClause = addAndClauseIfRequired(false, selectQuery);
+                    selectQuery.append(" ur.role_tenantid like ? ");
+                    preparedStatementValues.add( '%' +  userSearchCriteria.getTenantId().trim() + '%');
+                }
 
             }
         }
