80ba394f740bcbb9a20d532eaa277db064ea9c04
Rahul Dev Garg <118716448+gargrahuldev@users.noreply.github.com>
2023-12-26 13:58:08 +0530

    Update urls.dart

--- frontend/mgramseva/lib/services/urls.dart
+++ frontend/mgramseva/lib/services/urls.dart
@@ -3,7 +3,7 @@ class Url {
 
   static const String LOCALIZATION = 'localization/messages/v1/_search';
 
-  static const String MDMS = 'egov-mdms-service/v2/_search';
+  static const String MDMS = 'mdms-v2/v1/_search';
   static const String FILE_UPLOAD = 'filestore/v1/files';
   static const String FILE_FETCH = 'filestore/v1/files/url';
   static const String URL_SHORTNER = 'egov-url-shortening/shortener';
