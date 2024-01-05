5cb49d50b289d9bdc388bafc07c9c2ca25779152
Saloni-eGov <saloni.bajaj@egovernments.org>
2024-01-05 14:48:48 +0530

    PFM-4082 Echallan report

--- municipal-services/echallan-services/src/main/java/org/egov/echallan/repository/ChallanRepository.java
+++ municipal-services/echallan-services/src/main/java/org/egov/echallan/repository/ChallanRepository.java
@@ -518,8 +518,8 @@ public class ChallanRepository {
 		   }
 
 		   log.info("Query of expense bill report " +expenseBillQuery.toString()+" prepared statement "+preparedStatement);
-           List<ExpenseBillReportData> expenseBillReportData=new ArrayList<>();
-		   List<ExpenseBillReportData> expenseBillReportDataList=jdbcTemplate.query(expenseBillQuery.toString(), preparedStatement.toArray(),expenseBillReportRowMapper);
+           List<ExpenseBillReportData> expenseBillReportDataList=new ArrayList<>();
+		   expenseBillReportDataList=jdbcTemplate.query(expenseBillQuery.toString(), preparedStatement.toArray(),expenseBillReportRowMapper);
 		   return expenseBillReportDataList;
 	}
 }
