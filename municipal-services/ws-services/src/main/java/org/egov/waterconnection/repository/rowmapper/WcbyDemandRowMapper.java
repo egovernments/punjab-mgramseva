bc924cc46a68c831a837f13c9263735a4428691a
debasishchakraborty-egovt <debasish@egovernmets.org>
2024-01-23 10:06:02 +0530

    PFM-5473: Added changes to get the count of tenantby last demand gennerated date

--- municipal-services/ws-services/src/main/java/org/egov/waterconnection/repository/rowmapper/WcbyDemandRowMapper.java
+++ municipal-services/ws-services/src/main/java/org/egov/waterconnection/repository/rowmapper/WcbyDemandRowMapper.java
@@ -3,12 +3,13 @@ package org.egov.waterconnection.repository.rowmapper;
 import org.egov.waterconnection.web.models.WaterConnectionByDemandGenerationDate;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
+@Component
 public class WcbyDemandRowMapper implements ResultSetExtractor<List<WaterConnectionByDemandGenerationDate>> {
     @Override
     public List<WaterConnectionByDemandGenerationDate> extractData(ResultSet rs) throws SQLException, DataAccessException {
