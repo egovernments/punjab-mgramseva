34895a1c9e834e8f56eba6c28f6b9bf5cd48d04e
Saloni-eGov <saloni.bajaj@egovernments.org>
2024-02-07 12:50:13 +0530

    PFM-4855

--- municipal-services/ws-calculator/src/main/java/org/egov/wscalculation/service/DemandNotificationService.java
+++ municipal-services/ws-calculator/src/main/java/org/egov/wscalculation/service/DemandNotificationService.java
@@ -42,7 +42,7 @@ public class DemandNotificationService {
 			enrichSMSRequest(notificationObj, smsRequests, topic);
 			if (!CollectionUtils.isEmpty(smsRequests)) {
 				if(config.isSmsForDemandEnable()) {
-//					util.sendSMS(smsRequests);
+					util.sendSMS(smsRequests);
 				}
 			}
 		}
