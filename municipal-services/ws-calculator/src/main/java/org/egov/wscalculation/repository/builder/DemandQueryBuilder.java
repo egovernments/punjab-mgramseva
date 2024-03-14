7c28ce4225872ee76b8586b01a89d55111ca8e27
debasishchakraborty-egovt <debasish@egovernmets.org>
2024-03-14 20:02:41 +0530

    PFM-5638: Added change in Query

--- municipal-services/ws-calculator/src/main/java/org/egov/wscalculation/repository/builder/DemandQueryBuilder.java
+++ municipal-services/ws-calculator/src/main/java/org/egov/wscalculation/repository/builder/DemandQueryBuilder.java
@@ -15,13 +15,13 @@ import java.util.Set;
 @Component
 @Slf4j
 public class DemandQueryBuilder {
-    private static String selectClause = "SELECT b.demandid FROM egbs_demanddetail_v1 b ";
-    private static String subQuery = "SELECT dt.demandid FROM egbs_demand_v1 d LEFT OUTER JOIN egbs_demanddetail_v1 dt ON d.id = dt.demandid AND dt.taxamount > dt.collectionamount " +
+    private final static String selectClause = "SELECT b.demandid FROM egbs_demanddetail_v1 b ";
+    private final static String subQuery = "SELECT dt.demandid FROM egbs_demand_v1 d LEFT OUTER JOIN egbs_demanddetail_v1 dt ON d.id = dt.demandid AND dt.taxamount > dt.collectionamount " +
             "AND dt.taxheadcode = '10101'"+"AND d.status = 'ACTIVE'";
-    private static String firstWhereClause = "WHERE demandid IN (" ;
-    private static String secondWhereClause = ") AND b.tenantid = '";
+    private final static String firstWhereClause = "WHERE demandid IN (" ;
+    private final static String secondWhereClause = ") AND b.tenantid = '";
 
-    private static String groupByClause = " GROUP BY b.demandid " +
+    private final  static String groupByClause = " GROUP BY b.demandid " +
             "HAVING COUNT(*) = 1 ";
 
     public String getPenaltyQuery(String tenantId, Long penaltyThresholdDate, Integer daysToBeSubstracted ) {
@@ -30,15 +30,14 @@ public class DemandQueryBuilder {
         long tenDaysAgoMillis = Instant.ofEpochMilli(currentTimeMillis)
                 .minus(daysToBeSubstracted, ChronoUnit.DAYS)
                 .toEpochMilli();
-        subQuery = subQuery +  " AND d.tenantid = '"+tenantId+"'";
-        subQuery = subQuery + " AND d.createdtime < " + tenDaysAgoMillis;
-        if(!ObjectUtils.isEmpty(penaltyThresholdDate) && penaltyThresholdDate > 0) {
-            subQuery = subQuery + " AND d.taxperiodfrom > " + penaltyThresholdDate;
-        }
-        secondWhereClause= secondWhereClause +tenantId+"'";
-        firstWhereClause = firstWhereClause + subQuery + secondWhereClause + groupByClause;
-        selectClause = selectClause + firstWhereClause;
-        StringBuilder  builder = new StringBuilder(selectClause);
+        String subStringQuery ;
+        subStringQuery = subQuery +  " AND d.tenantid = '"+tenantId+"'" +
+                " AND d.createdtime < " + tenDaysAgoMillis + " AND d.taxperiodfrom > " + penaltyThresholdDate;
+        String firstStringWhereClause;
+        firstStringWhereClause = firstWhereClause + subStringQuery + secondWhereClause + tenantId+"'"+ groupByClause;
+        String query ;
+        query = selectClause + firstStringWhereClause;
+        StringBuilder  builder = new StringBuilder(query);
         log.info("Query formed :" + builder.toString());
         return builder.toString();
 
