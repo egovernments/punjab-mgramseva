94ad270c127569a07186c01e1f8bb0f113d42854
Nipun Arora <aroranipun1@gmail.com>
2024-06-25 23:17:46 +0530

    Update OpenSearch.js

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/payment/src/components/OpenSearch.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/payment/src/components/OpenSearch.js
@@ -8,65 +8,65 @@ const OpenSearch = () => {
   const queryParams = Digit.Hooks.useQueryParams();
 
   //An effect to update configs 
-  useEffect(() => {
-    // if (!queryParams.tenantId) {
-    //   // Update configs
-    //   OpenSearchConfig.minParametersForSearchForm += 1;
-    //   OpenSearchConfig.sections.search.uiConfig.minReqFields += 1;
-    //   OpenSearchConfig.sections.search.uiConfig.defaultValues = {
-    //     ...OpenSearchConfig.sections.search.uiConfig.defaultValues,
-    //     tenantId: ""
-    //   };
-    //   OpenSearchConfig.sections.search.uiConfig.fields = [
-    //     ...OpenSearchConfig.sections.search.uiConfig.fields,
-    //     {
-    //       label: "SELECT_TENANT",
-    //       type: "dropdown",
-    //       isMandatory: false,
-    //       disable: false,
-    //       populators: {
-    //         name: "tenantId",
-    //         optionsKey: "name",
-    //         optionsCustomStyle: { top: "2.3rem" },
-    //         mdmsConfig: {
-    //           masterName: "tenants",
-    //           moduleName: "tenant",
-    //           localePrefix: "TENANT",
-    //         },
-    //       },
-    //     },
-    //   ];
-    // }
+  // useEffect(() => {
+  //   // if (!queryParams.tenantId) {
+  //   //   // Update configs
+  //   //   OpenSearchConfig.minParametersForSearchForm += 1;
+  //   //   OpenSearchConfig.sections.search.uiConfig.minReqFields += 1;
+  //   //   OpenSearchConfig.sections.search.uiConfig.defaultValues = {
+  //   //     ...OpenSearchConfig.sections.search.uiConfig.defaultValues,
+  //   //     tenantId: ""
+  //   //   };
+  //   //   OpenSearchConfig.sections.search.uiConfig.fields = [
+  //   //     ...OpenSearchConfig.sections.search.uiConfig.fields,
+  //   //     {
+  //   //       label: "SELECT_TENANT",
+  //   //       type: "dropdown",
+  //   //       isMandatory: false,
+  //   //       disable: false,
+  //   //       populators: {
+  //   //         name: "tenantId",
+  //   //         optionsKey: "name",
+  //   //         optionsCustomStyle: { top: "2.3rem" },
+  //   //         mdmsConfig: {
+  //   //           masterName: "tenants",
+  //   //           moduleName: "tenant",
+  //   //           localePrefix: "TENANT",
+  //   //         },
+  //   //       },
+  //   //     },
+  //   //   ];
+  //   // }
 
-    if (!queryParams.businessService) {
-      // Update configs
-      OpenSearchConfig.minParametersForSearchForm += 1;
-      OpenSearchConfig.sections.search.uiConfig.minReqFields += 1;
-      OpenSearchConfig.sections.search.uiConfig.defaultValues = {
-        ...OpenSearchConfig.sections.search.uiConfig.defaultValues,
-        businessService: ""
-      };
-      OpenSearchConfig.sections.search.uiConfig.fields = [
-        ...OpenSearchConfig.sections.search.uiConfig.fields,
-        {
-          label: "SELECT_BS",
-          type: "dropdown",
-          isMandatory: false,
-          disable: false,
-          populators: {
-            name: "businessService",
-            optionsKey: "name",
-            optionsCustomStyle: { top: "2.3rem" },
-            mdmsConfig: {
-              masterName: "BusinessService",
-              moduleName: "BillingService",
-              localePrefix: "BUSINESS_SERV",
-            },
-          },
-        },
-      ];
-    }
-  }, []); 
+  //   if (!queryParams.businessService) {
+  //     // Update configs
+  //     OpenSearchConfig.minParametersForSearchForm += 1;
+  //     OpenSearchConfig.sections.search.uiConfig.minReqFields += 1;
+  //     OpenSearchConfig.sections.search.uiConfig.defaultValues = {
+  //       ...OpenSearchConfig.sections.search.uiConfig.defaultValues,
+  //       businessService: ""
+  //     };
+  //     OpenSearchConfig.sections.search.uiConfig.fields = [
+  //       ...OpenSearchConfig.sections.search.uiConfig.fields,
+  //       {
+  //         label: "SELECT_BS",
+  //         type: "dropdown",
+  //         isMandatory: false,
+  //         disable: false,
+  //         populators: {
+  //           name: "businessService",
+  //           optionsKey: "name",
+  //           optionsCustomStyle: { top: "2.3rem" },
+  //           mdmsConfig: {
+  //             masterName: "BusinessService",
+  //             moduleName: "BillingService",
+  //             localePrefix: "BUSINESS_SERV",
+  //           },
+  //         },
+  //       },
+  //     ];
+  //   }
+  // }, []); 
 
   return (
     <React.Fragment>
