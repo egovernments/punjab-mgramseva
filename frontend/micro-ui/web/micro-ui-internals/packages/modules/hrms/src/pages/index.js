1d55071e663f1aca59bd29ed81fcf5d1e3617025
Hari-egov <hariprasad@egovernments.org>
2024-06-28 17:38:55 +0530

    Back btn Icon added

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/hrms/src/pages/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/hrms/src/pages/index.js
@@ -42,11 +42,7 @@ const EmployeeApp = ({ path, url, userType }) => {
             </Link>{" "}
             / <span>{location.pathname ===  `/${window?.contextPath}/employee/hrms/inbox` ? t("HR_COMMON_HEADER") : t("HR_COMMON_HEADER")}</span>
           </p>
-          <button className="clear-search" style={{ marginLeft: "15px",
-            marginBottom: "5px",
-            padding: "8px",
-            borderRadius: "5px" }}  onClick={() => history.goBack()}>{t("HR_COMMON_BACK_ROUTE")}</button>
-          
+          <div class="back-btn2 " onClick={() => history.goBack()}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="19px"><path d="M24 0v24H0V0h24z" fill="none" opacity=".87"></path><path d="M14 7l-5 5 5 5V7z"></path></svg><p>Back</p></div>          
           <PrivateRoute
             path={`${path}/inbox`}
             component={() => (
