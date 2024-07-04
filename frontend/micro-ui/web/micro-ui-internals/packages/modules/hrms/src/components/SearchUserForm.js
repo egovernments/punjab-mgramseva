edadf1e745853af785e91e61e99df6537ac28b91
Nipun Arora <aroranipun1@gmail.com>
2024-07-04 17:43:41 +0530

    Search User scren draft one

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/hrms/src/components/SearchUserForm.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/hrms/src/components/SearchUserForm.js
@@ -1,7 +1,47 @@
-import { Loader, Header, Dropdown, LabelFieldPair, CardLabel, LinkLabel, SubmitBar } from "@egovernments/digit-ui-react-components";
-import React, { useState,useMemo,useEffect } from "react";
+import { Loader, Header, Dropdown, LabelFieldPair, CardLabel, LinkLabel, SubmitBar, Toast } from "@egovernments/digit-ui-react-components";
+import React, { useState, useMemo, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { Controller, useForm, useWatch } from "react-hook-form";
+import MultiSelectDropdown from "./MultiSelectDropdown";
+function filterKeys(data, keys) {
+  return data.map((item) => {
+    const filteredItem = {};
+    keys.forEach((key) => {
+      if (item.hasOwnProperty(key)) {
+        filteredItem[key] = item[key];
+      }
+    });
+    return filteredItem;
+  });
+}
+
+function getUniqueLeafCodes(tree) {
+  const codes = new Set();
+
+  function traverse(node) {
+    if (!node || typeof node !== "object") return;
+
+    const keys = Object.keys(node).filter((key) => key !== "options" && key !== "codes");
+
+    // Check if it's a leaf node (all remaining keys' values are strings)
+    const isLeafNode = keys.every((key) => typeof node[key] === "string");
+
+    if (isLeafNode && node.code) {
+      codes.add(node.code);
+    } else {
+      // Traverse every other key except options and codes
+      keys.forEach((key) => {
+        if (typeof node[key] === "object") {
+          traverse(node[key]);
+        }
+      });
+    }
+  }
+
+  traverse(tree);
+
+  return Array.from(codes);
+}
 
 function buildTree(data, hierarchy) {
   const tree = { options: [] };
@@ -12,7 +52,7 @@ function buildTree(data, hierarchy) {
 
     let currentLevel = tree;
 
-    hierarchy.forEach(({level}, index) => {
+    hierarchy.forEach(({ level }, index) => {
       const value = item[level];
 
       if (!currentLevel[value]) {
@@ -39,29 +79,28 @@ function buildTree(data, hierarchy) {
   return tree;
 }
 
-const SearchUserForm = () => {
+const SearchUserForm = ({ uniqueTenants, setUniqueTenants, roles, setUniqueRoles }) => {
   const { t } = useTranslation();
+  const [showToast, setShowToast] = useState(null);
   const [hierarchy, setHierarchy] = useState([
-    { "level": "zoneCode", "value": 1,"optionsKey":"zoneName" },
-    { "level": "circleCode", "value": 2,"optionsKey":"circleName" },
-    { "level": "divisionCode", "value": 3,"optionsKey":"divisionName" },
-    { "level": "subDivisionCode", "value": 4,"optionsKey":"subDivisionName" },
-    { "level": "sectionCode", "value": 5,"optionsKey":"sectionName" },
+    { level: "zoneCode", value: 1, optionsKey: "zoneName", isMandatory: true },
+    { level: "circleCode", value: 2, optionsKey: "circleName", isMandatory: true },
+    { level: "divisionCode", value: 3, optionsKey: "divisionName", isMandatory: true },
+    { level: "subDivisionCode", value: 4, optionsKey: "subDivisionName", isMandatory: false },
+    { level: "sectionCode", value: 5, optionsKey: "sectionName", isMandatory: false },
     // { "level": "schemeCode", "value": 6,"optionsKey":"schemeName" },
-    { "level": "code", "value": 7,"optionsKey":"name" }
-]
-);
-  
+    { level: "code", value: 7, optionsKey: "name", isMandatory: false },
+  ]);
   const [tree, setTree] = useState(null);
-  const [zones,setZones] = useState([])
-  const [circles,setCircles] = useState([])
-  const [divisions,setDivisions] = useState([])
-  const [subDivisions,setSubDivisions] = useState([])
-  const [sections,setSections] = useState([])
-  const [schemes,setSchemes] = useState([])
-  const [codes,setCodes] = useState([])
-  
-  console.log("tree", tree);
+  const [rolesOptions,setRolesOptions] = useState(null)
+  // const [zones,setZones] = useState([])
+  // const [circles,setCircles] = useState([])
+  // const [divisions,setDivisions] = useState([])
+  // const [subDivisions,setSubDivisions] = useState([])
+  // const [sections,setSections] = useState([])
+  // const [schemes,setSchemes] = useState([])
+  // const [codes,setCodes] = useState([])
+
   const requestCriteria = {
     url: "/mdms-v2/v1/_search",
     params: { tenantId: Digit.ULBService.getStateId() },
@@ -77,14 +116,40 @@ const SearchUserForm = () => {
               },
             ],
           },
+          {
+            moduleName: "ACCESSCONTROL-ROLES",
+            masterDetails: [
+              {
+                name: "roles",
+              },
+            ],
+          }
         ],
       },
     },
     config: {
       cacheTime: Infinity,
       select: (data) => {
-        const result = data?.MdmsRes?.tenant?.tenants
-        const resultInTree = buildTree(result, hierarchy);
+        const requiredKeys = [
+          "code",
+          "name",
+          "zoneCode",
+          "zoneName",
+          "circleCode",
+          "circleName",
+          "divisionCode",
+          "divisionName",
+          "subDivisionCode",
+          "subDivisionName",
+          "sectionCode",
+          "sectionName",
+          "schemeCode",
+          "schemeName",
+        ];
+        const result = data?.MdmsRes?.tenant?.tenants;
+        const filteredResult = filterKeys(result, requiredKeys);
+        const resultInTree = buildTree(filteredResult, hierarchy);
+        setRolesOptions(data?.MdmsRes?.["ACCESSCONTROL-ROLES"]?.roles)
         setTree(resultInTree);
         return result;
       },
@@ -108,14 +173,31 @@ const SearchUserForm = () => {
     clearErrors,
     unregister,
   } = useForm({
-    defaultValues: {},
+    defaultValues: {
+      "zoneCode": "",
+      "circleCode": "",
+      "divisionCode": "",
+      "subDivisionCode": "",
+      "sectionCode": "",
+      "code": "",
+      "roles": []
+  },
   });
 
   const formData = watch();
-  console.log("formData from watch",formData);
 
   const clearSearch = () => {
-    reset({});
+    reset({
+      "zoneCode": "",
+      "circleCode": "",
+      "divisionCode": "",
+      "subDivisionCode": "",
+      "sectionCode": "",
+      "code": "",
+      "roles": []
+  });
+    setUniqueRoles(null);
+    setUniqueTenants(null);
 
     // dispatch({
     //   type: uiConfig?.type === "filter"?"clearFilterForm" :"clearSearchForm",
@@ -131,33 +213,73 @@ const SearchUserForm = () => {
   };
 
   const onSubmit = (data) => {
-    console.log("formData", data);
-  };
+    //assuming atleast one hierarchy is entered
 
-  // const optionsForHierarchy = (hierarchy) =>{
-  //   // this gets called whenever formData changes
-  //   // Here we need to add logic to return options for one dropdown and return [] for all the childs
-  //   //zoneCode -> circleCode -> divisionCode -> subDivisionCode -> sectionCode -> schemeCode -> code
-  //   // console.log(hierarchy);
-  //   const data = getValues("zoneCode")
-  //   return tree.options
-  //   debugger
-  //   if(data){
-  //     return tree?.[data?.zoneCode]?.options
-  //   }
+    if (Object.keys(data).length === 0 || Object.values(data).every((value) => !value)) {
+      //toast message
+      setShowToast({ warning: true, label: t("ES_COMMON_MIN_SEARCH_CRITERIA_MSG") });
+      setTimeout(closeToast, 5000);
+      return;
+    }
+    //other validations if any
+    //check mandatory fields
+    let areMandatoryFieldsNotFilled = false;
+    hierarchy.forEach(({ level, isMandatory }) => {
+      if (isMandatory && (!data[level] || data[level]?.length === 0)) {
+        areMandatoryFieldsNotFilled = true;
+        return; // Exit the loop early
+      }
+    });
     
-  //   return null
-  // }
+    if (areMandatoryFieldsNotFilled) {
+      setShowToast({ warning: true, label: t("ES_COMMON_MIN_SEARCH_CRITERIA_MSG") });
+      setTimeout(closeToast, 5000);
+      return;
+    }
+
+    //checking roles
+    if(data?.roles?.length === 0 || !data?.roles){
+      setShowToast({ warning: true, label: t("ES_COMMON_MIN_SEARCH_CRITERIA_MSG") });
+      setTimeout(closeToast, 5000);
+      return;
+    }
+
+    //here apply a logic to compute the subtree based on the hierarchy selected
+    const levels = hierarchy.map(({ level }) => level);
+    //compute current level
+    let maxSelectedLevel = levels[0];
+    levels.forEach((level) => {
+      if (formData[level]) {
+        maxSelectedLevel = level;
+      } else {
+        return;
+      }
+    });
+
+    const levelIndex = levels.indexOf(maxSelectedLevel);
+
+    let currentLevel = tree;
+    for (let i = 0; i <= levelIndex; i++) {
+      const code = data?.[levels[i]]?.[levels[i]];
+      if (!code || !currentLevel[code]) return [];
+      currentLevel = currentLevel[code];
+    }
+
+    //this is the list of tenants under the current subtree
+    const listOfUniqueTenants = getUniqueLeafCodes(currentLevel);
+    setUniqueTenants(() => listOfUniqueTenants);
+    setUniqueRoles(() => data?.roles?.map(role=> role.code));
+  };
 
   const optionsForHierarchy = (level, value) => {
     if (!tree) return [];
-    
-    const levels = hierarchy.map(({level}) => level)
+
+    const levels = hierarchy.map(({ level }) => level);
     const levelIndex = levels.indexOf(level);
 
     //zoneCode(1st level(highest parent))
-    if (levelIndex === -1 || levelIndex=== 0) return tree.options;
-    
+    if (levelIndex === -1 || levelIndex === 0) return tree.options;
+
     let currentLevel = tree;
     for (let i = 0; i < levelIndex; i++) {
       const code = formData[levels[i]]?.[levels[i]];
@@ -167,40 +289,48 @@ const SearchUserForm = () => {
     return currentLevel.options || [];
   };
 
-  const renderFields = useMemo(() => { return hierarchy.map(({level,optionsKey,...rest},idx) => (
-    <LabelFieldPair>
-      <CardLabel style={{ marginBottom: "0.4rem" }}>{t(Digit.Utils.locale.getTransformedLocale(`HR_SU_${level}`))}</CardLabel>
-      <Controller
-        render={(props) => (
-          <Dropdown
-            style={{ display: "flex", justifyContent: "space-between" }}
-            option={optionsForHierarchy(level)}
-            key={level}
-            optionKey={optionsKey}
-            value={props.value}
-            select={(e) => {
-              props.onChange(e)
-              //clear all child levels
-              const childLevels = hierarchy.slice(hierarchy.findIndex(h => h.level === level) + 1);
-              childLevels.forEach(child => setValue(child.level, ""));
-            }}
-            selected={props.value}
-            defaultValue={props.value}
-            t={t}
-            optionCardStyles={{
-              top: "2.3rem",
-              overflow: "auto",
-              maxHeight: "200px",
-            }}
-          />
-        )}
-        rules={{}}
-        defaultValue={""}
-        name={level}
-        control={control}
-      />
-    </LabelFieldPair>
-  ))}, [formData])
+  const closeToast = () => {
+    setShowToast(null);
+  };
+
+  const renderHierarchyFields = useMemo(() => {
+    return hierarchy.map(({ level, optionsKey, isMandatory, ...rest }, idx) => (
+      <LabelFieldPair>
+        <CardLabel style={{ marginBottom: "0.4rem" }}>{`${t(Digit.Utils.locale.getTransformedLocale(`HR_SU_${level}`))} ${
+          isMandatory ? "*" : ""
+        }`}</CardLabel>
+        <Controller
+          render={(props) => (
+            <Dropdown
+              style={{ display: "flex", justifyContent: "space-between" }}
+              option={optionsForHierarchy(level)}
+              key={level}
+              optionKey={optionsKey}
+              value={props.value}
+              select={(e) => {
+                props.onChange(e);
+                //clear all child levels
+                const childLevels = hierarchy.slice(hierarchy.findIndex((h) => h.level === level) + 1);
+                childLevels.forEach((child) => setValue(child.level, ""));
+              }}
+              selected={props.value}
+              defaultValue={props.value}
+              t={t}
+              optionCardStyles={{
+                top: "2.3rem",
+                overflow: "auto",
+                maxHeight: "200px",
+              }}
+            />
+          )}
+          rules={{}}
+          defaultValue={""}
+          name={level}
+          control={control}
+        />
+      </LabelFieldPair>
+    ));
+  }, [formData]);
 
   if (isLoading || !setTree) {
     return <Loader />;
@@ -212,18 +342,70 @@ const SearchUserForm = () => {
         <div>
           <p className="search-instruction-header">{t("HR_SU_HINT")}</p>
           <div className={`search-field-wrapper search `}>
-            {renderFields}
+            {renderHierarchyFields}
+            <LabelFieldPair>
+              <CardLabel style={{ marginBottom: "0.4rem" }}>{`${t(Digit.Utils.locale.getTransformedLocale(`HR_SU_ROLES`))} ${"*"}`}</CardLabel>
+              <Controller
+                render={(props) => {
+                  return (
+                    <div style={{ display: "grid", gridAutoFlow: "row" }}>
+                      <MultiSelectDropdown
+                        options={rolesOptions}
+                        optionsKey={"name"}
+                        props={props} //these are props from Controller
+                        isPropsNeeded={true}
+                        onSelect={(e) => {
+                          props.onChange(
+                            e
+                              ?.map((row) => {
+                                return row?.[1] ? row[1] : null;
+                              })
+                              .filter((e) => e)
+                          )
+                        }}
+                        selected={props?.value || []}
+                        defaultLabel={t("HR_SU_SELECT_ROLES")}
+                        defaultUnit={ t("COMMON_ROLES_SELECTED")}
+                        showSelectAll={true}
+                        t={t}
+                        // config={config}
+                        // disable={false}
+                        // optionsDisable={config?.optionsDisable}
+                      />
+                    </div>
+                  )
+                }}
+                rules={{}}
+                defaultValue={[]}
+                name={"roles"}
+                control={control}
+              />
+            </LabelFieldPair>
             <div className={`search-button-wrapper search `} style={{}}>
-              <LinkLabel style={{ marginBottom: 0, whiteSpace: "nowrap" }} onClick={()=>{
-                clearSearch()
-              }}>
-                {t("CLEAR_SEARCH")}
+              <LinkLabel
+                style={{ marginBottom: 0, whiteSpace: "nowrap" }}
+                onClick={() => {
+                  clearSearch();
+                }}
+              >
+                {t("HR_SU_CLEAR_SEARCH")}
               </LinkLabel>
-              <SubmitBar label={t("SEARCH")} submit="submit" disabled={false} />
+              <SubmitBar label={t("HR_SU_SEARCH")} submit="submit" disabled={false} />
             </div>
           </div>
         </div>
       </form>
+      {showToast && (
+        <Toast
+          warning={showToast?.warning}
+          error={showToast?.error}
+          label={showToast?.label}
+          onClose={() => {
+            closeToast();
+          }}
+          isDleteBtn={true}
+        />
+      )}
     </div>
   );
 };
--- frontend/micro-ui/web/micro-ui-internals/packages/modules/hrms/src/components/SearchUserResults.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/hrms/src/components/SearchUserResults.js
@@ -1,9 +1,114 @@
 import React from 'react'
+import { useTranslation } from "react-i18next";
+import { Table,Loader,Card } from "@egovernments/digit-ui-react-components";
+import { Link } from "react-router-dom";
 
-const SearchUserResults = () => {
-  return (
-    <div>SearchUserResults</div>
-  )
+const SearchUserResults = ({isLoading,data,...props}) => {
+  const { t } = useTranslation();
+  const GetCell = (value) => <span className="cell-text">{t(value)}</span>;
+  const GetSlaCell = (value) => {
+    return value == "INACTIVE" ? (
+      <span className="sla-cell-error">{t(value) || ""}</span>
+    ) : (
+      <span className="sla-cell-success">{t(value) || ""}</span>
+    );
+  };
+
+  const columns = React.useMemo(() => {
+    return [
+      {
+        Header: t("HR_EMP_ID_LABEL"),
+        disableSortBy: false,
+        accessor: "code",
+        Cell: ({ row }) => {
+          return (
+            <span className="link">
+              <Link to={`/${window?.contextPath}/employee/hrms/details/${row.original.tenantId}/${row.original.code}`}>{row.original.code}</Link>
+            </span>
+            // GetCell(`${row.original?.code}`)
+          );
+        },
+      },
+      {
+        Header: t("HR_EMP_NAME_LABEL"),
+        disableSortBy: false,
+        accessor: "name",
+        Cell: ({ row }) => {
+          return GetCell(`${row.original?.user?.name}`);
+        },
+      },
+      {
+        Header: t("HR_USER_ID_LABEL"),
+        disableSortBy: false,
+        accessor: "mobileNumber",
+        Cell: ({ row }) => {
+          return GetCell(`${row.original?.user?.mobileNumber}`);
+        },
+      },
+      {
+        Header: t("HR_STATUS_LABEL"),
+        disableSortBy: false,
+        accessor: "isActive",
+        Cell: ({ row }) => {
+          return GetSlaCell(`${row.original?.isActive ? "ACTIVE" : "INACTIVE"}`);
+        },
+      },
+    ];
+  }, [data]);
+
+  let result;
+
+  if (isLoading) {
+    result = <Loader />;
+  } else if (data?.length === 0) {
+    result = (
+      <Card style={{ marginTop: 20 }}>
+        {/* TODO Change localization key */}
+        {t("COMMON_TABLE_NO_RECORD_FOUND")
+          .split("\\n")
+          .map((text, index) => (
+            <p key={index} style={{ textAlign: "center" }}>
+              {text}
+            </p>
+          ))}
+      </Card>
+    );
+  } else if (data?.length > 0) {
+    result = (
+      <Table
+      t={t}
+      data={data}
+      columns={columns}
+      getCellProps={(cellInfo) => {
+        return {
+          style: {
+            maxWidth: cellInfo.column.Header == t("HR_EMP_ID_LABEL") ? "150px" : "",
+            padding: "20px 18px",
+            fontSize: "16px",
+            minWidth: "150px",
+          },
+        };
+      }}
+      // onNextPage={onNextPage}
+      // onPrevPage={onPrevPage}
+      // currentPage={currentPage}
+      totalRecords={data ? data.length : 0}
+      // onPageSizeChange={onPageSizeChange}
+      // pageSizeLimit={pageSizeLimit}
+      // onSort={onSort}
+      // sortParams={sortParams}
+      // disableSort={disableSort}
+      autoSort={true}
+      manualPagination={false}
+      />
+    );
+  }
+
+    return (
+      <div>
+        {result}
+      </div>
+    )
 }
 
 export default SearchUserResults
