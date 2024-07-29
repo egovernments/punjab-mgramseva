2de47037a7f2ef65239e1adf9ab166b53cfe18bc
Saloni <103189799+saloni041@users.noreply.github.com>
2024-07-29 14:52:20 +0530

    Update app.py

--- utilities/rollout-dashboard-cronjob-indexer/app.py
+++ utilities/rollout-dashboard-cronjob-indexer/app.py
@@ -19,6 +19,7 @@ def getGPWSCHeirarchy():
     try:
         mdms_url = os.getenv('API_URL')
         state_tenantid = os.getenv('TENANT_ID')
+        url=os.getenv('IFIX_DEP_ENTITY_URL')
         mdms_requestData = {
             "RequestInfo": {
                 "apiId": "mgramseva-common",
@@ -56,7 +57,7 @@ def getGPWSCHeirarchy():
             if tenant.get('city') is not None and tenant.get('city').get('code') is not None:
                 teanant_data_Map.update({tenant.get('city').get('code'): tenant.get('code')})
 
-        url = 'https://mgramseva-dwss.punjab.gov.in/'
+        # url = 'https://mgramseva-dwss.punjab.gov.in/'
         print(url)
         requestData = {
             "requestHeader": {
