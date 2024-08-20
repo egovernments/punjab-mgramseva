295296c8b1cf714ca2e9323c8a591d888c6095d8
Hari-egov <hariprasad@egovernments.org>
2024-08-20 14:18:49 +0530

    ISTE - 363,383,388,379,366

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/TopBar.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/TopBar.js
@@ -55,7 +55,7 @@ const TopBar = ({
             className="city"
             id="topbar-logo"
             src={paymentlogoUrl || "https://cdn.jsdelivr.net/npm/@egovernments/digit-ui-css@1.0.7/img/m_seva_white_logo.png"}
-            alt="mSeva"
+            alt="mGramSeva"
           />
           {isPaymentPath && <img className="state" src={logoUrl} />}
           {!isPaymentPath && <h3>{cityOfCitizenShownBesideLogo}</h3>}
