429bc2bbe6602a52c3ce7e2ee1907fd5a88b46dc
Taniya-eGov <taniyapushey@gmail.com>
2024-08-26 13:40:43 +0530

    updated createdtime

--- municipal-services/ws-calculator/src/main/resources/db/migration/ddl/V20240826000600__wsc_bulk_create_ddl.sql
+++ municipal-services/ws-calculator/src/main/resources/db/migration/ddl/V20240826000600__wsc_bulk_create_ddl.sql
@@ -1,7 +1,7 @@
 
 ALTER TABLE eg_ws_bulk_demand_batch ALTER COLUMN createdTime DROP DEFAULT;
 
-ALTER TABLE eg_ws_bulk_demand_batch ALTER COLUMN createdTime TYPE BIGINT;
+ALTER TABLE eg_ws_bulk_demand_batch ALTER COLUMN createdTime TYPE BIGINT USING createdTime::BIGINT;
 
 ALTER TABLE eg_ws_bulk_demand_batch ALTER COLUMN lastModifiedBy TYPE VARCHAR(64);
 
