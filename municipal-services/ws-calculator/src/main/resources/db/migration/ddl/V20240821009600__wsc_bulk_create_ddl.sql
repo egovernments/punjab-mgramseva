4d26fd92c20931a333da226b4b4b89e91f121da4
Taniya-eGov <taniyapushey@gmail.com>
2024-08-26 09:58:31 +0530

    Updated createdtime configuration

--- municipal-services/ws-calculator/src/main/resources/db/migration/ddl/V20240821009600__wsc_bulk_create_ddl.sql
+++ municipal-services/ws-calculator/src/main/resources/db/migration/ddl/V20240821009600__wsc_bulk_create_ddl.sql
@@ -1,20 +1,21 @@
-CREATE TABLE IF NOT EXISTS eg_ws_bulk_demand_batch
-(
+CREATE TABLE IF NOT EXISTS eg_ws_bulk_demand_batch (
   id VARCHAR(64) PRIMARY KEY,
   tenantId VARCHAR(64) NOT NULL,
   billingPeriod VARCHAR(64) NOT NULL,
-  createdTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status VARCHAR(50) NOT NULL
-  createdBy character varying(64) NOT NULL,
-  lastModifiedBy bigint NOT NULL,
-  lastModifiedTime bigint,
+  createdTime BIGINT NOT NULL,
+  status VARCHAR(50) NOT NULL,
+  createdBy VARCHAR(64) NOT NULL,
+  lastModifiedBy VARCHAR(64) NOT NULL,
+  lastModifiedTime BIGINT
 );
+
 CREATE SEQUENCE seq_eg_ws_bulk_demand_batch
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
+
 ALTER TABLE eg_ws_bulk_demand_batch ALTER COLUMN id SET DEFAULT nextval('seq_eg_ws_bulk_demand_batch'::regclass);
 
 CREATE INDEX IF NOT EXISTS index_eg_ws_bulk_demand_batch_tenantId ON eg_ws_bulk_demand_batch (tenantId);
